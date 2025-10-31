b390eec436b475a2dba87bf79b7fd1ff16360612
Ahmed Tawfik <ahmedtawfik833@gmail.com>
2025-10-31 15:25:25 +0200

    [1768] [Time Beats: 71.22%] [Memory Beats: 8.39%] - LeetPush

--- Merge-Strings-Alternately.py
+++ Merge-Strings-Alternately.py
@@ -1,12 +1,11 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        p1 = p2 = 0
+        p1 = 0
         res = ""
-        while p1 < len(word1) or p2 < len(word2):
+        while p1 < len(word1) or p1 < len(word2):
             if p1 < len(word1):
                 res += word1[p1]
-                p1+=1
-            if p2 < len(word2):
-                res += word2[p2]
-                p2+=1
+            if p1 < len(word2):
+                res += word2[p1]
+            p1+=1
         return res
