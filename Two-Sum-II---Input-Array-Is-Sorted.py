5f4627d1b123fc3670cd35710dc7754ea4683ead
Ahmed Tawfik <ahmedtawfik833@gmail.com>
2025-11-05 21:12:14 +0200

    [167] [Time Beats: 5.11%] [Memory Beats: 9.30%] - LeetPush

---
+++ Two-Sum-II---Input-Array-Is-Sorted.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        l, r = 0, len(nums)-1
+        while l<r:
+            cur = nums[l] + nums[r]
+            if(cur < target):
+                l+=1
+            elif(cur==target):
+                return [l+1,r+1]
+            else:
+                r-=1
+            print(l, r)
